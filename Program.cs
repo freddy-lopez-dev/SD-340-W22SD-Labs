ee4785949f8d9896b417956932055c925d11c58d
Frederick Lopez <mtester010@gmail.com>
2022-09-07 13:49:43 -0500

    Added a delegate for string

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using System.Text;
+
+static string AppendString(string wordArg, int numArg)
+{
+    StringBuilder sb = new StringBuilder();
+
+    for(int i = 0; i < numArg; i++)
+    {
+        sb.Append(wordArg);
+    }
+    return sb.ToString();
+};
+
+
+static string RemoveChars(string wordArg, int numArg)
+{
+    return wordArg.Substring(0, wordArg.Length - numArg);
+}
+
+StringDel newDel = new StringDel(RemoveChars);
+
+Console.WriteLine(newDel("mynewstring", 4));
+newDel = PrintWord;
+Console.WriteLine(newDel("cat", 3));
+
+public delegate string StringDel(string wordArg, int numArg);
+
