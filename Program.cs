66fc831f904071027311a9d8bf4d66aacaef9295
Frederick Lopez <mtester010@gmail.com>
2022-09-09 23:01:47 -0500

    Added linkedlist sample

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿LinkedList<int> list = new LinkedList<int> ();
+list.Add(2);
+
+list.PrintAll();
+
+
+class LinkedList<T>
+{
+    Node head;
+    public class Node
+    {
+        public T Data { get; set; }
+        public Node next;
+    }
+
+    public void Add(T data)
+    {
+        Node node = new Node ();
+        node.Data = data;
+    }
+
+    
+    public void PrintAll()
+    {
+        Node n = head;
+        while(n != null)
+        {
+            Console.WriteLine(n.Data);
+            n = n.next;
+        }
+    }
+
+    public void Print()
+    {
+        Node node = head;
+        Console.WriteLine(node.Data);
+    }
+
+}
