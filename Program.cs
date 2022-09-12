07b625cf2097f231f50d3e98223433e4e21bb55a
Frederick Lopez <mtester010@gmail.com>
2022-09-12 17:58:11 -0500

    Fix the class node to have the head and assign the value of data

--- Program.cs
+++ Program.cs
@@ -1,24 +1,38 @@
 ﻿LinkedList<int> list = new LinkedList<int> ();
 list.Add(2);
+list.Add(3);
+list.Add(5);
 
 list.PrintAll();
+list.Print();
 
 
-class LinkedList<T>
+class LinkedList <T>
 {
-    Node head;
+    public Node head;
     public class Node
     {
         public T Data { get; set; }
-        public Node next;
+        public Node next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            this.next = null;
+        }
     }
 
     public void Add(T data)
     {
-        Node node = new Node ();
-        node.Data = data;
+        Node node = new Node (data);
+        if(head == null)
+        {
+            head = node;
+            return;
+        }
+        node.next = head;
+        head = node;
     }
-
     
     public void PrintAll()
     {
