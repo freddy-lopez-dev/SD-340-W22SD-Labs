3678e70733718b6aa55b344e1fc47387015cbfaa
Frederick Lopez <mtester010@gmail.com>
2022-09-15 16:58:23 -0500

    Add initial endpoint for testing

--- Program.cs
+++ Program.cs
@@ -15,4 +15,31 @@ using (var scope = app.Services.CreateScope())
     await SeedData.Initialize(services);
 }
 
+#region Route 
+
+app.MapGet("/route", async (int? Number, SD_340_W22SD_Lab_3Context db) =>
+{
+    return await db.Routes.Include(r => r.StopSchedules).ToListAsync();
+});
+
+#endregion
+
+#region Stop 
+
+app.MapGet("/stop", async (int? Number, SD_340_W22SD_Lab_3Context db) =>
+{
+    return await db.Stop.Include(r => r.StopSchedules).ToListAsync();
+});
+
+#endregion
+
+#region StopSchedule 
+
+app.MapGet("/stopschedule", async (int? Number, int top, SD_340_W22SD_Lab_3Context db) =>
+{
+    return await db.ScheduledStops.ToListAsync();
+});
+
+#endregion
+
 app.Run();
