4eb407628359ad3b3fb3c74bdae018f49ff72943
Frederick Lopez <mtester010@gmail.com>
2022-09-23 16:45:16 -0500

    Using Directory, StreamReader and StreamWriter

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System.Text;
+
+string directory = @"C:\Users\Slickrick\Documents";
+
+StringBuilder sb = new StringBuilder();
+
+try
+{
+    //Set the current directory.
+    Directory.SetCurrentDirectory(directory);
+    string[] files = Directory.GetFiles(directory,"theMachineStops.txt");
+    using (StreamReader sr = File.OpenText(files[0]))
+    {
+        sb.Append(sr.ReadToEnd());
+    }
+
+    sb.Replace(".", "STOP");
+
+    string fileName = "TelegramCopy.txt";
+
+    using (StreamWriter sw = File.CreateText(System.IO.Path.Combine(directory, fileName)))
+    {
+        sw.Write(sb.ToString());
+    }
+
+
+}
+catch (DirectoryNotFoundException e)
+{
+    Console.WriteLine("The specified directory does not exist. {0}", e);
+}
+
+Console.WriteLine(Directory.GetCurrentDirectory());
+
